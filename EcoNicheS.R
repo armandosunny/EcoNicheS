d5f0bdedeb205a3be6c2a5df8fbc13c289a12bd3
Armando Sunny <sunny.biologia@gmail.com>
2024-01-14 15:08:51 -0600

    Update EcoNicheS.R

--- EcoNicheS.R
+++ EcoNicheS.R
@@ -1,3 +1,8 @@
+This is a Shiny web application. You can run the application by clicking the 'Run App' button above.
+
+Find out more about building applications with Shiny here:
+
+http://shiny.rstudio.com/
 
 options(shiny.maxRequestSize = 6000*1024^2)
  
