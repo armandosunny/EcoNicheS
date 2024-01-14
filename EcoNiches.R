e9fa92ecb0f6a87da8732e77f247c133b0f4a5a4
Armando Sunny <sunny.biologia@gmail.com>
2024-01-14 01:55:40 -0600

    Update EcoNiches.R

--- EcoNiches.R
+++ EcoNiches.R
@@ -3,3 +3,19 @@ This is a Shiny web application. You can run the application by clicking the 'Ru
 Find out more about building applications with Shiny here:
 
 http://shiny.rstudio.com/
+
+
+install_version("shiny", version = "1.7.5")
+install_version("terra", version = "1.7.46")
+install_version("usdm", version = "2.1.6")
+install_version("ENMTools", version = "1.1.1")	
+install_version("biomod2", version = "4.2.4")
+install_version("RColorBrewer", version = "1.1.3")
+install_version("dismo", version = "1.3.14")
+install_version("tiff", version = "0.1.11")
+install_version("rJava", version = "1.0.6")
+install_version("tidyterra", version = "0.4.0")
+install_version("shinydashboard", version = "0.7.2")
+install_version("pROC", version = "1.18.4")
+install_version("R.utils", version = "2.12.2")
+install_version("ENMGadgets", version = "0.1.0.1")
