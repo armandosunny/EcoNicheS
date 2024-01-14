4f726bc370473d3f7ef4f9603b2e3d189999bdcb
Armando Sunny <sunny.biologia@gmail.com>
2024-01-14 01:54:29 -0600

    Create EcoNicheS.R

---
+++ EcoNiches.R
@@ -0,0 +1,5 @@
+This is a Shiny web application. You can run the application by clicking the 'Run App' button above.
+
+Find out more about building applications with Shiny here:
+
+http://shiny.rstudio.com/
